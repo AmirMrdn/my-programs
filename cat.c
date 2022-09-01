92d3d26c32c52ecadd9acd1a8beeca380384b01e
AmirMrdn <91540422+AmirMrdn@users.noreply.github.com>
2022-08-31 22:37:43 -0700

    add cat program

--- cat.c
+++ cat.c
@@ -1,5 +1,15 @@
-int main(){
+#include <stdlib.h>
+#include <stdio.h>
 
-
-  retunr 0;
+ int main(int argc,char **argv){
+ 
+  FILE *fp = fopen(argv[1],"r");
+  if(!fp){puts("can not open file");exit(1);}
+  
+  while(!feof(fp)) 
+   printf("%c",fgetc(fp));
+  
+   fclose(fp);
+  
+  return 0;
  }
